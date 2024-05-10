4ffb8c2c9c780802ddce9dd9d86b683d5e97d449
YmCris <ymcrisystems@gmail.com>
2024-05-09 23:33:53 -0600

    Implementando paquetes extras

---
+++ PRE-IPC-1-EXERCISES/PRE-IPC-1-EXERCISES/src/main/java/com/ymcris/pre/ipc1/conceptos/h/palabrasreservadas/PalabraStatic.java
@@ -0,0 +1,9 @@
+package com.ymcris.pre.ipc1.conceptos.h.palabrasreservadas;
+
+/**
+ *
+ * @author crist
+ */
+public class PalabraStatic {
+
+}
--- PRE-IPC-1-EXERCISES/PRE-IPC-1-EXERCISES/src/main/java/com/ymcris/pre/ipc1/logica/a/calculadorasimple/Main.java
+++ PRE-IPC-1-EXERCISES/PRE-IPC-1-EXERCISES/src/main/java/com/ymcris/pre/ipc1/logica/a/calculadorasimple/Main.java
@@ -14,5 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("BIENVENIDO A LIMALAYA");
+        System.out.println("HOLA JUAN");
     }
 }

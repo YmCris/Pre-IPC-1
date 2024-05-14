ef265faccbd909923c0ca2d41846a51755561a10
YmCris <ymcrisystems@gmail.com>
2024-05-13 18:07:18 -0600

    Nuevo ejercicio

---
+++ 1. Java/PRE-IPC-1/PRE-IPC-1-EXERCISES/src/main/java/com/ymcris/pre/ipc1/logica/calculadorasimple/Main.java
@@ -0,0 +1,19 @@
+package com.ymcris.pre.ipc1.logica.calculadorasimple;
+
+/*
+DESARROLLA UNA CALCULADORA EN CONSOLA EN LA CUAL EL USUARIO
+INTRODUZCA DOS DIGITOS Y LA OPERACIÓN A REALIZAR (SUMA,
+RESTA, MULTIPLICACIÓN Y DIVISIÓN)
+ */
+/**
+ *
+ * @author crist
+ * @version 21.0.2
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDO A LIMALAYA");
+        System.out.println("HOLA JUAN");
+    }
+}
---
+++ 1. Java/PRE-IPC-1/PRE-IPC-1-EXERCISES/src/main/java/com/ymcris/pre/ipc1/logica/intercambiodevalores/IntercambioDeValores.java
@@ -0,0 +1,9 @@
+package com.ymcris.pre.ipc1.logica.intercambiodevalores;
+
+/**
+ *
+ * @author crist
+ */
+public class IntercambioDeValores {
+    
+}
